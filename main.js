2899a9059fe22e77b4ad771b014792366ae15919
Luisa <luisa.santos042@gmail.com>
2022-11-05 00:23:16 +0000

    add card generating functions with mock data

---
+++ main.js
@@ -0,0 +1,30 @@
+function createGame(player1, player2, time) {
+    return `
+      <li>
+        <img src="./assets/icon-${player1}.svg" alt="${player1}" />
+        <strong>${time}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="${player2}" />
+      </li>
+    `
+}
+
+function createCard(date, weekDay) {
+    return `
+    <div class="card">
+      <h2>${date} <span>${weekDay}</span></h2>
+      <ul>
+      ${createGame('brazil', 'serbia', '16:00')}
+      </ul>
+    </div>
+    `
+}
+
+document.querySelector('#app').innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW" />
+  </header>
+  <main id="cards">
+    ${createCard('24/11', 'quinta')}
+    ${createCard('25/11', 'sexta')}
+  </main>
+`;
